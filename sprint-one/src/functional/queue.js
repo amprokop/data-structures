58d0654f630e0ac0121d4cbf1f048ac2f1e5ebd4
Alex Prokop <alex.prokop@gmail.com>
2013-09-25 14:57:04 -0700

    Finishes stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,43 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var indexFirst = 0;
+  var indexLast = 0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[indexLast] = value;
+    //at the new indexLast, set value
+    indexLast = indexLast + 1;
+    //expand the index to prepare for a new value
   };
 
   instance.dequeue = function(){
+    var value = storage[indexFirst]; 
+    //save the value for output
+    storage[indexFirst] = undefined;
+    //get rid of the value of the first value
+    indexFirst = indexFirst + 1;
+    //shift the index forward one
+    return value;
+    //give us the value
+    
   };
 
   instance.size = function(){
+    if ((indexLast - indexFirst) > 0) {
+    return indexLast - indexFirst;
+  } else {
+    return 0;
+  }
+  
   };
 
   return instance;
 };
+
+//two indexes--first and last. when you enqeue an item, start using fir
+
+//size is now last minus first.
+
+//the 
