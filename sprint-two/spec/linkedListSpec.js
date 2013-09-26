04819ff38c03c4f919036b3682db9169d0e7a5ca
Hack Reactor Students <github@hackreactor.com>
2013-09-26 11:19:59 -0700

    Adds tests for all methods.

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -15,6 +15,33 @@ describe("linkedList", function() {
     expect(linkedList.removeHead).toEqual(jasmine.any(Function));
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
-
   // add more tests here to test the functionality of linkedList
+  it("should return correct value of head and tail after adding to tail", function() {
+    linkedList.addToTail(3);
+    expect(linkedList.head.value).toEqual(3);
+    expect(linkedList.tail.value).toEqual(3);
+  });
+
+  it("should return correct value after removing head", function() {
+    linkedList.addToTail(3);
+    linkedList.addToTail('hello');
+    linkedList.addToTail(true);
+    expect(linkedList.removeHead()).toEqual(3);
+  });
+
+  it("should return true if list contains a value that has been added", function() {
+    linkedList.addToTail(3);
+    linkedList.addToTail('hello');
+    linkedList.addToTail(true);
+    expect(linkedList.contains('hello')).toEqual(true);
+    expect(linkedList.contains(3)).toEqual(true);
+    expect(linkedList.contains(true)).toEqual(true);
+  });
+
+  it("should return false if list does not contain a value", function() {
+    linkedList.addToTail(3);
+    linkedList.addToTail('hello');
+    linkedList.addToTail(true);
+    expect(linkedList.contains('world')).toEqual(false);
+  });
 });
