cac8ab3750ab352fbba19a489b27ede102f1e1ee
Hack Reactor <hackreactor@HR-A1.local>
2013-09-25 17:27:00 -0700

    Completed stack for prototypal.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,27 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var storage = Object.create(stackMethods);
+  storage.stackSize = 0;
+  return storage;
 };
 
+
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this[this.stackSize]=value;
+  this.stackSize++;
+};
+
+stackMethods.pop = function(){
+  this[this.stackSize] = undefined;
+  if (this.stackSize){
+    this.stackSize--;
+  }
+  return this[this.stackSize];
+};
+
+stackMethods.size = function(){
+  return this.stackSize;
+};
+
 
