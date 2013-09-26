abe9f7af12849bc2e4223057c7647a10f969b5d6
Hack Reactor <hackreactor@HR-A1.local>
2013-09-25 20:43:54 -0700

    Completed  pseudoclassical queue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,24 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.indexFirst = 0;
+  this.indexLast = 0;
+};
+
+
+Queue.prototype.enqueue = function(value){
+  this[this.indexLast] = value;
+  this.indexLast = this.indexLast + 1;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.size()===0) {return undefined};
+
+  var value = this[this.indexFirst];
+  this[this.indexFirst] = undefined;
+  this.indexFirst = this.indexFirst + 1;
+  return value;
+};
+
+Queue.prototype.size = function(){
+  return this.indexLast - this.indexFirst;
 };
 
