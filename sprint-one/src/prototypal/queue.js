a4df5f0d956416345e029777a50967321d7b7151
Hack Reactor <hackreactor@HR-A1.local>
2013-09-25 17:26:22 -0700

    Completed queue for prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,34 @@
 var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var storage = Object.create(queueMethods);
+  storage.indexFirst = 0;
+  storage.indexLast = 0;
+
+  return storage;
 };
 
 var queueMethods = {};
 
+
+queueMethods.enqueue = function(value){
+  this[this.indexLast] = value;
+  //at the new this.indexLast, set value
+  this.indexLast = this.indexLast + 1;
+  //expand the this.index to prepare for a new value
+};
+
+queueMethods.dequeue = function(){
+  if(this.size()===0) {return undefined};
+
+  var value = this[this.indexFirst];
+  //save the value for output
+  this[this.indexFirst] = undefined;
+  //get rid of the value of the first value
+  this.indexFirst = this.indexFirst + 1;
+  //shift the this.index forward one
+  return value;
+  //give us the value
+};
+
+queueMethods.size = function(){
+  return this.indexLast - this.indexFirst;
+};
