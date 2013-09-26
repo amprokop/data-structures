5418b016866d63235c18dbb438eea6e16713efee
Hack Reactor Students <github@hackreactor.com>
2013-09-26 11:18:46 -0700

    Adds tests, completes LinkedList basic requirements.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,35 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    var newTail = makeNode(value);
+    if(list.head === null) {
+      list.head = newTail;
+    } else {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
+
   };
 
   list.removeHead = function(){
+    var headVal = list.head.value;
+    var next = list.head.next;
+    delete list.head;
+    list.head = next;
+    return headVal;
   };
 
-  list.contains = function(){
+  list.contains = function(target, node){
+    node = node || list.head;
+    if(node.value === target) {
+      return true;
+    }
+    if(node.next === null) {
+      return false;
+    } else {
+      return list.contains(target, node.next);
+    }
   };
 
   return list;
