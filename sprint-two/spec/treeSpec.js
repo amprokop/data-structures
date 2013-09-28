1331389fec5f66fbac64c4bdbd6a49b9770cd625
Hack Reactor Students <github@hackreactor.com>
2013-09-28 10:59:28 -0700

    Adds tests for tree extra credit.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -21,4 +21,31 @@ describe("tree", function() {
     expect(tree.contains(2)).toBe(true);
   });
 
+  it("should be able to completely remove a tree from its parent, keeping all children trees intact", function(){
+    tree.addChild(5);
+    tree.addChild(8);
+    tree.addChild(9);
+    expect(tree.children[0].value).toEqual(5);
+    expect(tree.children[1].value).toEqual(8);
+    expect(tree.children[2].value).toEqual(9);
+    tree.children[0].addChild(8);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(12);
+    tree.children[1].addChild(89);
+    tree.children[2].addChild(56);    
+    expect(tree.children[0].parent).toEqual(tree);
+    expect(tree.children[1].parent).toEqual(tree);
+    expect(tree.children[0].children[0].parent).toEqual(tree.children[0]);
+    expect(tree.children[1].children[0].parent).toEqual(tree.children[1]);
+
+    var runaway = tree.children[0].children[0];
+    runaway.removeFromParent();
+
+    expect(runaway.parent).toBeUndefined();
+    expect(tree.children[0].children[0].value).toEqual(7);
+
+
+
+  });
+
 });
